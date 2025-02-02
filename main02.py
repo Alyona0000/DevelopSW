22c6844982c9db6d74f959726123b375e9c5e065
LadyLyo <alena.rybakova61@gmail.com>
2025-02-02 19:24:32 +0200

    fhfhhf

--- main02.py
+++ main02.py
@@ -1,1 +1,18 @@
-print("*_*")
+#print("*_*")
+def status():
+    print("Hello, World:D")
+def vivaMechMat():
+    print("Viva Mech-mat faculty!!!")
+s=0
+a=10
+while s<a :
+    s=s+1
+    status()   
+
+####
+s=0
+a=10 
+while s<a :
+    s=s+1
+    vivaMechMat() 
+fffffffffffffffffffffffffff
